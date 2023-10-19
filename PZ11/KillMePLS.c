e64a7d651b3f0d0412db96a248c5727963400d1d
chelovek sverh <senior.nikita.adamovitch@ya.ru>
2023-10-19 22:25:46 +0500

    pz_11

--- PZ11/KillMePLS.c
+++ PZ11/KillMePLS.c
@@ -5,17 +5,34 @@ void printRegionAddress(LPVOID lpAddress, SIZE_T dwSize) {
     printf("Region Address: %p - %p\n", lpAddress, (char*)lpAddress + dwSize - 1);
 }
 
+void printErrorMessage(DWORD error) {
+    LPVOID errorMsg;
+    FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+        NULL,
+        error,
+        0, // Default language
+        (LPWSTR)&errorMsg,
+        0,
+        NULL
+    );
+
+    wprintf(L"Error: %s", (LPWSTR)errorMsg);
+
+    LocalFree(errorMsg);
+}
+
 int main() {
     SYSTEM_INFO sysInfo;
     GetSystemInfo(&sysInfo);
     SIZE_T dwPageSize = sysInfo.dwPageSize;
 
-
     LPVOID lpRegion1 = VirtualAlloc(NULL, 2 * dwPageSize, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
     LPVOID lpRegion2 = VirtualAlloc(NULL, 2 * dwPageSize, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
 
     if (lpRegion1 == NULL || lpRegion2 == NULL) {
-        fprintf(stderr, "Error reserving memory\n");
+        fprintf(stderr, "Error reserving memory: ");
+        printErrorMessage(GetLastError());
         return GetLastError();
     }
 
@@ -23,9 +40,7 @@ int main() {
     printRegionAddress(lpRegion1, 2 * dwPageSize);
     printRegionAddress(lpRegion2, 2 * dwPageSize);
 
-
     ZeroMemory(lpRegion1, 2 * dwPageSize);
-
 
     int number;
     printf("Enter an integer between 0 and 127: ");
@@ -43,23 +58,47 @@ int main() {
         return -1;
     }
 
+    DWORD oldProtect;
+    if (VirtualProtect(lpRegion1, 2 * dwPageSize, PAGE_NOACCESS, &oldProtect)) {
+        printf("\nRegion 1 access is now completely restricted.\n");
+    }
+    else {
+        fprintf(stderr, "Error protecting memory: ");
+        printErrorMessage(GetLastError());
+    }
 
     int fillValue = number;
     FillMemory(lpRegion2, 2 * dwPageSize, fillValue);
 
     printf("\nContents of the regions:\n");
-    for (char* ptr = (char*)lpRegion1; ptr < (char*)lpRegion1 + 2 * dwPageSize; ptr++) {
-        printf("%02X ", *ptr);
+    __try {
+        for (char* ptr = (char*)lpRegion1; ptr < (char*)lpRegion1 + 2 * dwPageSize; ptr++) {
+            printf("%02X ", *ptr);
+        }
     }
-    printf("\n");
-    for (char* ptr = (char*)lpRegion2; ptr < (char*)lpRegion2 + 2 * dwPageSize; ptr++) {
-        printf("%02X ", *ptr);
+    __except (EXCEPTION_EXECUTE_HANDLER) {
+        fprintf(stderr, "\nException caught while accessing memory: ");
+        printErrorMessage(GetLastError());
     }
     printf("\n");
 
+    __try {
+        for (char* ptr = (char*)lpRegion2; ptr < (char*)lpRegion2 + 2 * dwPageSize; ptr++) {
+            printf("%02X ", *ptr);
+        }
+    }
+    __except (EXCEPTION_EXECUTE_HANDLER) {
+        fprintf(stderr, "\nException caught while accessing memory: ");
+        printErrorMessage(GetLastError());
+    }
+    printf("\n");
+
+    VirtualProtect(lpRegion1, 2 * dwPageSize, oldProtect, &oldProtect);
 
     VirtualFree(lpRegion1, 0, MEM_RELEASE);
     VirtualFree(lpRegion2, 0, MEM_RELEASE);
+
+    printf("Completed\n");
 
     return 0;
 }
